cd01da2a3026c9e2856ab979d0e6ab4a06a97ca0
arkarsg <alxarkar@gmail.com>
2024-01-05 18:28:49 +0800

    Remove commented code

--- voyage-app/app/(public)/login.tsx
+++ voyage-app/app/(public)/login.tsx
@@ -4,14 +4,7 @@ import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import Spinner from "react-native-loading-spinner-overlay";
 
-import {
-  Form,
-  Button,
-  YStack,
-  Input,
-  Text,
-  XStack,
-} from "tamagui";
+import { Form, Button, YStack, Input, Text, XStack } from "tamagui";
 import { orange, purple } from "@tamagui/themes";
 
 const login = () => {
@@ -66,11 +59,7 @@ const login = () => {
   };
 
   return (
-    <YStack
-      overflow="hidden"
-      flex={1}
-      backgroundColor={orange.orange1}
-    >
+    <YStack overflow="hidden" flex={1} backgroundColor={orange.orange1}>
       <Spinner visible={loading} />
       <Form onSubmit={onSignInPress}>
         <YStack alignItems="center" paddingTop="$8" space="$1">
@@ -132,98 +121,17 @@ const login = () => {
         </YStack>
       </Form>
       <XStack alignSelf="center">
-        <Text fontFamily="$body">Don't have an account?{" "}</Text>
-        <Text fontFamily="Inter_600SemiBold" color={purple.purple9} onPress={handleCreateAccountPress}>Join</Text>
+        <Text fontFamily="$body">Don't have an account? </Text>
+        <Text
+          fontFamily="Inter_600SemiBold"
+          color={purple.purple9}
+          onPress={handleCreateAccountPress}
+        >
+          Join
+        </Text>
       </XStack>
     </YStack>
   );
-
-  // return (
-  //   <View className="h-full w-full flex bg-stone-100">
-  //     <Spinner visible={loading} />
-  //     <View className="flex items-center space-y-8 pt-32">
-  //       {/* Email text input */}
-  //       <View className="space-y-2 w-full items-center">
-  //         <View className="justify-around items-left border-b border-stone-300 w-10/12">
-  //           <TextInput
-  //             autoCapitalize="none"
-  //             placeholder="E-mail"
-  //             value={emailAddress}
-  //             onChangeText={setEmailAddress}
-  //             style={{
-  //               marginVertical: 15,
-  //               fontFamily: "IBMPlexSans_500Medium",
-  //               fontSize: 15,
-  //             }}
-  //           />
-  //         </View>
-  //         {/* Password input */}
-  //         <View className="justify-around items-left border-b border-stone-300 w-10/12">
-  //           <TextInput
-  //             placeholder="Password"
-  //             value={password}
-  //             onChangeText={setPassword}
-  //             secureTextEntry
-  //             style={{
-  //               marginVertical: 15,
-  //               fontFamily: "IBMPlexSans_500Medium",
-  //               fontSize: 15,
-  //             }}
-  //           />
-  //         </View>
-
-  //         <Pressable
-  //           className="flex items-end p-3 justify-end space-y-5 ml-60"
-  //           onPress={handleResetPress}
-  //         >
-  //           <Text
-  //             style={{
-  //               fontFamily: "IBMPlexSans_400Regular",
-  //             }}
-  //             className="text-voyage-blue tracking-tightest text-m"
-  //           >
-  //             Forgot password?
-  //           </Text>
-  //         </Pressable>
-  //       </View>
-
-  //       <Pressable
-  //         onPress={onSignInPress}
-  //         className="rounded-3xl w-10/12 items-center px-4 py-3 bg-voyage-blue"
-  //       >
-  //         <Text
-  //           style={{
-  //             fontFamily: "IBMPlexSans_500Medium",
-  //           }}
-  //           className="text-zinc-100 text-lg"
-  //         >
-  //           Log in
-  //         </Text>
-  //       </Pressable>
-  //     </View>
-
-  //     <View className="justify-center flex-row items-center m-5">
-  //       <Text
-  //         style={{
-  //           fontFamily: "IBMPlexSans_400Regular",
-  //         }}
-  //         className="text-zinc-700 text-m"
-  //       >
-  //         Don't have an account?{" "}
-  //       </Text>
-  //       <Pressable onPress={handleCreateAccountPress}>
-  //         <Text
-  //           style={{
-  //             fontFamily: "IBMPlexSans_400Regular",
-  //           }}
-  //           className="text-voyage-blue text-m"
-  //         >
-  //           Create Account
-  //         </Text>
-  //       </Pressable>
-  //     </View>
-  //   </View>
-  // );
 };
 
 export default login;
